2b6887c667c9dcd7be78b8ffbd1bf24791fcf1a7
usman jamil <usmanjamil.zamstudios@gmail.com>
2021-07-05 18:46:04 +0500

    test2

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,4 +1,4 @@
-const { verifySignUp } = require("../middlewares/varifySignup");
+const { verifySignUp } = require("../middlewares/verifySignup"));
 const controller = require("../controllers/auth.controller");
 const router = require("express").Router();
 
