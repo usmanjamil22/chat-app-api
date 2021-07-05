db292d48da6321e6e6fa6afc44c8cc0f2ede6253
usman jamil <usmanjamil.zamstudios@gmail.com>
2021-07-05 18:24:41 +0500

    clear error

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,4 +1,4 @@
-const { verifySignUp } = require("../middlewares");
+const { verifySignUp } = require("../middlewares/index.js");
 const controller = require("../controllers/auth.controller");
 const router = require("express").Router();
 
